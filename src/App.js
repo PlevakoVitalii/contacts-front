a52d33b3f7a0312ff8ab8cc897be48e4f008898d
Vitalii Plevako <vitalii.plevako@plus4u.net>
2023-10-21 14:57:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [user, setUser] = useState('Vasa');
+
+  return (
+    <>
+      <h1>Hello fullstack developer!!</h1>
+    </>
+  );
+}
+
+export default App;
+
